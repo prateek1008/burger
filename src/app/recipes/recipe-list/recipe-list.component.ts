c313e58c74c8d39d2cc725284fc9db896adf913d
Avinash <avinash20894@gmail.com>
2021-06-05 11:32:28 +0530

    Add: Added items in recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Chowmein',
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque provident.',
+      'https://www.viniscookbook.com/wp-content/uploads/2019/02/20190214_164218.jpg'
+    ),
+    new Recipe(
+      'Chowmein',
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque provident.',
+      'https://www.viniscookbook.com/wp-content/uploads/2019/02/20190214_164218.jpg'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
