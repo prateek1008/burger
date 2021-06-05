55d2b2ab04d3f81e8c29e85c5bb4414ac9a0beb4
Avinash <avinash20894@gmail.com>
2021-06-06 02:50:05 +0530

    Add: Basic setup done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients : Ingredient[] = [
+    new Ingredient("Apple", 5),
+    new Ingredient("Banana", 10)
+  ]
+
 
   constructor() { }
 
